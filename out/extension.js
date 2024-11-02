e66408fcc94b5386d8883e656d3d04c0f7dbe502
EinsZwo <CzechEinsZwo@gmail.com>
2024-11-01 21:49:06 -0600

    add macro support, add basic roleset tooltips

--- out/extension.js
+++ out/extension.js
@@ -1,14 +1,39 @@
 "use strict";
 // @ts-nocheck
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = activate;
 exports.deactivate = deactivate;
+const fs = __importStar(require("fs"));
 const vscode = require('vscode');
 const path = require('path');
 const os = require('os');
 const cp = require('child_process');
 let wordTooltipCache = {};
 let lastDocumentText = ''; // for caching results
+let rolesetCache = {};
 function getExecutablePath() {
     const platform = os.platform();
     const extensionPath = vscode.extensions.getExtension('mabu4315.umr-doc-helper').extensionPath;
@@ -41,6 +66,9 @@ function runExecutable() {
     });
 }
 function generateAlignedText(tiers, colWidths) {
+    if ((!tiers) || (!tiers.map)) {
+        return "";
+    }
     return tiers.map(tier => {
         return tier.map((word, colIndex) => {
             return word.padEnd(colWidths[colIndex], ' ');
@@ -52,42 +80,116 @@ function createMarkdownContent(alignedText) {
 ${alignedText}
 \`\`\``;
 }
+let macros = {};
+function loadMacros() {
+    const config = vscode.workspace.getConfiguration('macroExtension');
+    const userMacros = config.get('macros', {});
+    const defaultMacros = {
+        'p1s': '(p / person \n\t:refer-person 1\n\t:refer-number singular)',
+        'p1p': '(p / person \n\t:refer-person 1\n\t:refer-number plural)',
+        'p2s': '(p / person \n\t:refer-person 2\n\t:refer-number singular)',
+        'p2p': '(p / person \n\t:refer-person 2\n\t:refer-number plural)',
+        'p3s': '(p / person \n\t:refer-person 3\n\t:refer-number singular)',
+        'p3p': '(p / person \n\t:refer-person 3\n\t:refer-number plural)'
+    };
+    macros = { ...defaultMacros, ...userMacros };
+}
+loadMacros();
+// Watch for configuration changes
+vscode.workspace.onDidChangeConfiguration(event => {
+    if (event.affectsConfiguration('macroExtension.macros')) {
+        loadMacros();
+    }
+});
 function activate(context) {
-    console.log('umr-doc-helper is now active!');
+    console.log('UMR Annotation Helper is now active!');
+    const tooltipsPath = path.join(context.extensionPath, 'data', 'tooltips.json');
+    try {
+        const fileContents = fs.readFileSync(tooltipsPath, 'utf8');
+        rolesetCache = JSON.parse(fileContents);
+        console.log("Loaded roleset tooltips");
+        console.log(` Sample entry activate-01: '${rolesetCache['activate-01']}'`);
+    }
+    catch (err) {
+        vscode.window.showErrorMessage(`Failed to load tooltips: ${err.message}`);
+    }
     // Register a hover provider for all file types
-    let disposable = vscode.languages.registerHoverProvider({ scheme: 'file', language: '*' }, {
+    let hover_provider = vscode.languages.registerHoverProvider({ scheme: 'file', language: '*' }, {
         provideHover(document, position, token) {
             // Get the word at the hover position
             const range = document.getWordRangeAtPosition(position, /\w+/);
             const word = range ? document.getText(range) : '';
-            // If there's a word at the hover position, check the cache for tooltips
+            const possible_roleset_range = document.getWordRangeAtPosition(position, /[a-zA-Z][a-zA-Z\-]*-\d+/);
+            console.log(rolesetCache['activate-01']);
+            let roleset = '';
+            if (possible_roleset_range) {
+                roleset = document.getText(possible_roleset_range);
+                console.log(`roleset '${roleset}'`);
+            }
+            if (roleset) {
+                if (rolesetCache[roleset]) {
+                    const formattedTooltip = rolesetCache[roleset];
+                    const markdown = new vscode.MarkdownString(formattedTooltip);
+                    markdown.isTrusted = true;
+                    return new vscode.Hover(markdown);
+                }
+                else {
+                    console.log(`Couldn't find tooltip for roleset '${roleset}'.`);
+                }
+            }
             if (word) {
                 if (wordTooltipCache[word]) {
-                    console.log(`Tooltip from cache for word '${word}': ${wordTooltipCache[word]}`);
+                    //console.log(`Tooltip from cache for word '${word}': ${wordTooltipCache[word]}`);
                     const tooltipInfo = wordTooltipCache[word] || '';
                     if (!tooltipInfo) {
-                        return vscode.Hover(tooltipInfo);
+                        return null;
                     }
                     const [first_lines, gloss_lines, column_widths, last_lines, graph] = tooltipInfo;
-                    let tooltip = first_lines.join("\n") + "\n"
-                        + createMarkdownContent(generateAlignedText(gloss_lines, column_widths)) + "\n" // gloss lines get spaced padding and need a monospaced font
+                    let tooltip = "";
+                    const aligned_text = generateAlignedText(gloss_lines, column_widths);
+                    if (!aligned_text) {
+                        return null;
+                    }
+                    if (Array.isArray(first_lines)) {
+                        tooltip = first_lines.join("\n");
+                    }
+                    else if (typeof first_lines == 'string') {
+                        tooltip = first_lines;
+                    }
+                    tooltip = tooltip + "\n"
+                        + createMarkdownContent(aligned_text) + "\n" // gloss lines get spaced padding and need a monospaced font
                         + last_lines + "\n\n"
                         + graph.replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;').replace(/^\s+/gm, match => match.replace(/ /g, '&nbsp;'));
                     const formattedTooltip = tooltip
                         .split('\n') // Split the tooltip into individual lines
                         .map(line => line.replace(new RegExp(`\\b${word}\\b`, 'g'), `**${word}**`)) // Bold the hovered word on each line
-                        .join('  \n'); // Join lines with a soft line break (two spaces + \n)
+                        .join('  \n'); // Join w/ a soft line break
                     const markdown = new vscode.MarkdownString(formattedTooltip);
-                    markdown.isTrusted = true; // Ensure Markdown is trusted
+                    markdown.isTrusted = true;
                     return new vscode.Hover(markdown);
                 }
                 else {
-                    console.log(`No cached tooltip for '${word}', awaiting cache update.`);
+                    console.log(`No tooltip for available for '${word}'.`);
                 }
             }
         }
     });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(hover_provider);
+    // build initial mappings for the variable tooltips
+    const editor = vscode.window.activeTextEditor;
+    if (editor && editor.document) {
+        const document = editor.document;
+        const fullText = document.getText();
+        if (fullText !== lastDocumentText) {
+            lastDocumentText = fullText;
+            runPythonScriptForTooltips(fullText);
+        }
+        console.log("Building initial item mappings");
+    }
+    else {
+        console.log("No active editor on activation; skipping tooltip cache update");
+    }
+    // register handler for tooltip cache update
     vscode.workspace.onDidChangeTextDocument((event) => {
         const documentText = event.document.getText();
         if (documentText !== lastDocumentText) {
@@ -95,11 +197,58 @@ function activate(context) {
             runPythonScriptForTooltips(documentText);
         }
     });
-    if (vscode.window.activeTextEditor) {
-        const initialDocumentText = vscode.window.activeTextEditor.document.getText();
-        lastDocumentText = initialDocumentText;
-        runPythonScriptForTooltips(initialDocumentText);
-    }
+    console.log("Registering macros...");
+    let macro_command = vscode.commands.registerCommand('extension.expandMacro', async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        const document = editor.document;
+        const position = editor.selection.active;
+        // Get the text from the start of the line to the cursor
+        const lineText = document.lineAt(position.line).text.substr(0, position.character);
+        // Extract the last word (potential macro shortcut)
+        const match = lineText.match(/(\S+)\s*$/);
+        const shortcut = match ? match[1] : '';
+        const macroText = macros[shortcut];
+        if (macroText) {
+            // **Step 1: Get the current line's indentation**
+            const line = document.lineAt(position.line);
+            const lineIndentation = line.text.substr(0, line.firstNonWhitespaceCharacterIndex);
+            const macroLines = macroText.split('\n');
+            const indentedMacroLines = macroLines.map((lineContent, index) => {
+                if (index === 0) {
+                    // First line: use current indentation
+                    return lineContent;
+                }
+                else {
+                    // Subsequent lines: add indent level
+                    return lineIndentation + lineContent;
+                }
+            });
+            const finalMacroText = indentedMacroLines.join('\n');
+            // **Step 4: Replace the shortcut with the indented macro text**
+            const startPosition = position.translate(0, -shortcut.length);
+            const range = new vscode.Range(startPosition, position);
+            await editor.edit(editBuilder => {
+                editBuilder.replace(range, finalMacroText);
+            });
+            // **Optional: Move the cursor to the end of the inserted text**
+            const lastLine = startPosition.line + indentedMacroLines.length - 1;
+            const lastChar = indentedMacroLines[indentedMacroLines.length - 1].length;
+            const newPosition = new vscode.Position(lastLine, lastChar);
+            editor.selection = new vscode.Selection(newPosition, newPosition);
+        }
+        else {
+            // No macro found, insert a tab character
+            await vscode.commands.executeCommand('tab');
+        }
+    });
+    context.subscriptions.push(macro_command);
+    let editMacrosCommand = vscode.commands.registerCommand('macroExtension.editMacros', () => {
+        vscode.commands.executeCommand('workbench.action.openSettings', 'macroExtension.macros');
+    });
+    context.subscriptions.push(editMacrosCommand);
 }
 function runPythonScriptForTooltips(documentText) {
     return new Promise((resolve, reject) => {
@@ -119,7 +268,7 @@ function runPythonScriptForTooltips(documentText) {
             console.log('Python script ran.');
             try {
                 const result = JSON.parse(output);
-                console.log('Parsed result from Python:', result);
+                //console.log('Parsed result from Python:', result);
                 wordTooltipCache = result; // Update the cache with the new tooltips
                 resolve(); // Resolve when the cache is updated
             }
---
+++ src/generate_roleset_tooltips.py
@@ -0,0 +1,80 @@
+import xml.etree.ElementTree as ET
+import re
+import os
+import json
+
+
+def convert_roleset_to_markdown(xml_data):
+    root = ET.fromstring(xml_data)
+    roleset_id = root.attrib.get('id', '').replace(".", "-")
+    roleset_name = root.attrib.get('name', '')
+
+    markdown_lines = []
+
+    # Header: roleset id
+    markdown_lines.append(f"# {roleset_id}")
+
+    # Subheader: roleset name
+    markdown_lines.append(f"## {roleset_name}")
+    markdown_lines.append("")
+
+    # Aliases
+    aliases = root.find('aliases')
+    if aliases is not None and len(aliases):
+        markdown_lines.append("### Aliases")
+        markdown_lines.append("| Alias | POS |")
+        markdown_lines.append("|-------|-----|")
+        for alias in aliases.findall('alias'):
+            alias_text = alias.text or ''
+            alias_pos = alias.attrib.get('pos', '')
+            markdown_lines.append(f"| {alias_text} | {alias_pos} |")
+        markdown_lines.append("")
+
+    # Roles
+    roles = root.find('roles')
+    if roles is not None and len(roles):
+        markdown_lines.append("### Roles")
+        for role in roles.findall('role'):
+            n = role.attrib.get('n', '')
+            arg_label = f"**ARG{n}**"
+            descr = role.attrib.get('descr', '')
+            f = role.attrib.get('f', '')
+            # Italicize content in parentheses
+            descr = re.sub(r'\((.*?)\)', r'(*\1*)', descr)
+            f_str = f"(*{f}*)" if f else ''
+            # Combine descr and f
+            role_line = f"{arg_label}: {descr} {f_str}".strip()
+            markdown_lines.append(f"- {role_line}")
+        markdown_lines.append("")
+
+    # Combine all markdown lines into a single string
+    markdown_str = "\n".join(markdown_lines)
+    return markdown_str
+
+def process_rolesets(xml_data, tooltips):
+    root = ET.fromstring(xml_data)
+    for roleset in root.findall('.//roleset'):
+        roleset_id = roleset.attrib.get('id', '').replace(".","-")
+        # Convert roleset element to string
+        roleset_xml = ET.tostring(roleset, encoding='unicode')
+        markdown_str = convert_roleset_to_markdown(roleset_xml)
+        tooltips[roleset_id] = markdown_str
+    return tooltips
+
+
+def main():
+    tooltips = {}
+    dir = "../frames"
+    for file in os.listdir(dir):
+        if not file.endswith('.xml'):
+            continue
+        with open(os.path.join(dir, file), 'r', encoding='utf-8') as infile:
+            text = "\n".join(infile.readlines()[2:])
+            process_rolesets(text, tooltips)
+
+    with open("tooltips.json", 'w+', encoding='utf-8') as outfile:
+        json.dump(tooltips, outfile, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    main()
